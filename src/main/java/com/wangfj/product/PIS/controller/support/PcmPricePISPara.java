794d62e4740271a42543160f73ceb7037bb21c9b
kongqingfu <kongqingfu@wangfujing.com>
2016-10-12 13:56:50 +0800

    解决导入终端字段多的问题

--- src/main/java/com/wangfj/product/PIS/controller/support/PcmPricePISPara.java
+++ src/main/java/com/wangfj/product/PIS/controller/support/PcmPricePISPara.java
@@ -1,5 +1,8 @@
 package com.wangfj.product.PIS.controller.support;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PcmPricePISPara {
 
 	/**
